80590b411d1b3323354d01b24b88c2a0d6d12369
Eva van Geenen <eva.vangeenen@incentro.com>
2019-10-16 13:06:19 +0200

    Getting customer list with DAO, DAOImpl and injected session and DAO in customer controller

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,16 +1,37 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	@Autowired
+	CustomerDAO customerDAO;
 	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		
+		for(Customer customer : theCustomers){
+			System.out.println(customer.toString());
+		}
+		
+		theModel.addAttribute("customers", theCustomers);
+		
+		
+		
 		return "list-customers";
 	}
 }
---
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,47 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Transactional
+	@Override
+	public Customer getCustomer() {
+
+		return null;
+	}
+
+	
+	
+	@Transactional
+	@Override
+	public List<Customer> getCustomers() {
+		
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+														// In HQL query, don't write the table name but write your Entity class name in your query!
+		Query<Customer> theQuery = currentSession.createQuery("FROM Customer", Customer.class);
+		
+		List<Customer> customers = theQuery.getResultList();
+		
+		
+		return customers;
+	}
+	
+
+}
